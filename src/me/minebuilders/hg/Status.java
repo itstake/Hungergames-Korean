581e45fbcd44081daa444e3b0a062ec302fd6c91
itstake <bexco2010@gmail.com>
2014-08-22 01:51:49 +0900

    Status.java 한글화

--- src/me/minebuilders/hg/Status.java
+++ src/me/minebuilders/hg/Status.java
@@ -4,10 +4,10 @@ package me.minebuilders.hg;
 import org.bukkit.ChatColor;
 
 public enum Status {
-	RUNNING(ChatColor.GREEN  + "" + ChatColor.BOLD +  "Running"), STOPPED(ChatColor.DARK_RED  + "" + ChatColor.BOLD +  "Stopped"),
-	WAITING(ChatColor.AQUA  + "" + ChatColor.BOLD +  "Waiting..."), BROKEN(ChatColor.DARK_RED  + "" + ChatColor.BOLD +  "BROKEN"), 
-	ROLLBACK(ChatColor.RED  + "" + ChatColor.BOLD +  "Restoring..."), NOTREADY(ChatColor.DARK_BLUE  + "" + ChatColor.BOLD +  "NotReady"), 
-	BEGINNING(ChatColor.GREEN  + "" + ChatColor.BOLD +  "Running"), COUNTDOWN(ChatColor.AQUA  + "" + ChatColor.BOLD +  "Starting...");
+	RUNNING(ChatColor.GREEN  + "" + ChatColor.BOLD +  "게임중"), STOPPED(ChatColor.DARK_RED  + "" + ChatColor.BOLD +  "시작하지 않음"),
+	WAITING(ChatColor.AQUA  + "" + ChatColor.BOLD +  "사람을 기다리는 중..."), BROKEN(ChatColor.DARK_RED  + "" + ChatColor.BOLD +  "오류"), 
+	ROLLBACK(ChatColor.RED  + "" + ChatColor.BOLD +  "롤백중..."), NOTREADY(ChatColor.DARK_BLUE  + "" + ChatColor.BOLD +  "준비되지 않음"), 
+	BEGINNING(ChatColor.GREEN  + "" + ChatColor.BOLD +  "게임중"), COUNTDOWN(ChatColor.AQUA  + "" + ChatColor.BOLD +  "시작중...");
 
 	private String name;
 
