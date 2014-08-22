8942989b85f00a4d3bdb8f52a70916bd4f3162e2
itstake <bexco2010@gmail.com>
2014-08-23 08:19:36 +0900

    Update LeaveCmd.java

--- src/me/minebuilders/hg/commands/LeaveCmd.java
+++ src/me/minebuilders/hg/commands/LeaveCmd.java
@@ -15,7 +15,7 @@ public class LeaveCmd extends BaseCmd {
 	@Override
 	public boolean run() {
 		HG.plugin.players.get(player.getName()).getGame().leave(player);
-		Util.msg(player, "&cYou left Hungergames!");
+		Util.msg(player, "이 미니게임에서 나가셨습니다.");
 		return true;
 	}
 }
