3c702d90c30371dd299a8ccd66df76f52e206ff7
itstake <bexco2010@gmail.com>
2014-08-23 08:30:35 +0900

    Update KillManager.java

--- src/me/minebuilders/hg/managers/KillManager.java
+++ src/me/minebuilders/hg/managers/KillManager.java
@@ -8,29 +8,29 @@ public class KillManager {
 
 	public String getDeathString(DamageCause dc, String name) {
 		switch (dc) {
-		case ENTITY_EXPLOSION: return (name + " Was blown to bits!");
-		case BLOCK_EXPLOSION: return (name + " Was blown to bits!");
-		case CUSTOM: return (name + " Was killed by an unknown cause!");
-		case FALL: return (name + " Hit the ground too hard!");
-		case FALLING_BLOCK: return (name + " Was smashed by a block!");
-		case FIRE: return (name + " Was Burned alive!");
-		case FIRE_TICK: return (name + " Was Burned alive!");
-		case PROJECTILE: return (name + " Got hit by a projectile!");
-		case LAVA: return (name + " Fell into a pit of lava!");
-		case MAGIC: return (name + " Was destroyed by magic!");
-		case SUICIDE: return (name + " Couldn't handle hungergames!");
-		default: return (name + " Was killed by " + dc.toString().toLowerCase());
+		case ENTITY_EXPLOSION: return (name + " 님이 터져 죽었습니다!");
+		case BLOCK_EXPLOSION: return (name + " 님이 터져 죽었습니다!");
+		case CUSTOM: return (name + " 알수없는 이유로 죽었습니다!");
+		case FALL: return (name + " 땅에 떨어져 죽었습니다!");
+		case FALLING_BLOCK: return (name + " 님이 떨어지는 블록에 맞아 죽었습니다!");
+		case FIRE: return (name + " 님이 타 죽었습니다!");
+		case FIRE_TICK: return (name + " 님이 타 죽었습니다!");
+		case PROJECTILE: return (name + " 님이 던진것에 맞아 죽었습니다!");
+		case LAVA: return (name + " 용암에 빠져 죽었습니다!");
+		case MAGIC: return (name + " 마법으로 인해 죽었습니다!");
+		case SUICIDE: return (name + " 자살했습니다!");
+		default: return (name + " 님이 " + dc.toString().toLowerCase() + "님에게 죽었습니다!");
 		}
 	}
 
 	public String getKillString(String name, Entity e) {
 		switch (e.getType()) {
-		case PLAYER: return ("&3" + name + " &b&lWas killed by &3" + ((Player)e).getName() + " &busing a(n) &3" + ((Player)e).getItemInHand().getType().name().toLowerCase() + "&b!");
-		case ZOMBIE: return (name + " Was ripped to bits by a Zombie!");
-		case SKELETON: return (name + " Was shot in the face by a skeleton");
-		case ARROW: return (name + " Was shot in the face by a skeleton");
-		case SPIDER: return (name + " Was eaten alive by a Spider!");
-		default: return (name + " Spontaneously died!");
+		case PLAYER: return (name + " 님이 " + ((Player)e).getName() + " 님에게 " + ((Player)e).getItemInHand().getType().name().toLowerCase() + " 으로 죽었습니다!");
+		case ZOMBIE: return (name + " 님이 좀비에게 죽었습니다!");
+		case SKELETON: return (name + " 님이 스켈레톤에게 죽었습니다");
+		case ARROW: return (name + " 님이 스켈레톤에게 맞아 죽었습니다");
+		case SPIDER: return (name + " 님이 거미에게 맞아 죽었습니다!");
+		default: return (name + " 님이 죽었습니다!");
 		}
 	}
 }
